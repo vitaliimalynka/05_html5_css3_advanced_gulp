0a20b3eaaa10d1d3c41ef2652d0b31856b00f680
vitaliimalynka <vitalii.malynka@gmail.com>
2023-02-01 20:25:42 +0200

    init

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import submodule from "./submodule.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(`%c>>>>> OK! <<<<<`, 'color: blue; background-color: yellow')
+    submodule()
+})
